9ee4562008011b1faacc77bca564bf4656822f6d
danny_chen <danny_chen@seberon.com>
2025-04-17 18:06:32 +0800

    chore: 新增 .prettierignore 檔案以忽略建構產生的檔案及特定配置檔案，並更新 eslint.config.js 以讀取自動匯入的全域變數

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,15 @@
-import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+import fs from 'fs'
+import path from 'path'
+
+import js from '@eslint/js'
+import pluginVitest from '@vitest/eslint-plugin'
+import pluginVue from 'eslint-plugin-vue'
+
+const autoImportJson = JSON.parse(
+  fs.readFileSync(path.resolve('.eslintrc-auto-import.json'), 'utf8'),
+)
 
 export default [
   {
@@ -15,14 +23,26 @@ export default [
   },
 
   js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-
+  ...pluginVue.configs['flat/recommended'],
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
   skipFormatting,
   {
-    extends: ['./eslintrc-auto-import.json'],
+    languageOptions: {
+      globals: autoImportJson.globals,
+    },
+  },
+  {
+    rules: {
+      'vue/attribute-hyphenation': [
+        'off',
+        {
+          ignore: [],
+          ignoreTags: [],
+        },
+      ],
+    },
   },
 ]
--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import './styles/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import PrimeVue from 'primevue/config'
 
+import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
 
 import zhTW from 'primelocale/zh-TW'
+
 import App from './App.vue'
 import router from './router'
 
